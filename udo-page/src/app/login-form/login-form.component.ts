24c93546e1b4460f1cdb74ef71129977f83fa1a2
Unknown <shakerchabarekh@gmail.com>
2018-08-26 13:30:10 -0400

    Guardar usuario y clave
    Funciona pero no se por que aparece y desaparece la informacion de la consola

--- udo-page/src/app/login-form/login-form.component.ts
+++ udo-page/src/app/login-form/login-form.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-
+  public user = ' ';
+  public psw = ' ';
   constructor() { }
 
   ngOnInit() {
+  }
+  iniciarSesion(usuario: string, clave: string) {
+    if (usuario) {
+      this.user = usuario;
+    }
+    if (clave) {
+      this.psw = clave;
+    }
+    console.log(this.user);
+    console.log(this.psw);
   }
 
 }
